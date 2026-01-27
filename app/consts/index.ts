a68a9c6d02be149f766a3d9c0325e0878206f8cd
Neza Bacar <neza.bacar@gmail.com>
2026-01-27 14:25:49 +0100

    Updated locales

--- app/consts/index.ts
+++ app/consts/index.ts
@@ -1,5 +1,5 @@
 export const BALI_PRICE = "€3000";
-export const BALI_DATE = "6.3. - 13.3. 2026";
+export const BALI_DATE = "6.3. - 16.3. 2026";
 
 export const PAG_DATES = [
   "23.6. - 28.6.",
@@ -11,10 +11,10 @@ export const PAG_PRICING = "€850";
 
 export const SILBA_ONE_DIVE_PRICE = "€35";
 export const SILBA_ONE_DAY_PRICE = "€60";
-export const SILBA_FULL_COURSE_PRICE = "€160";
+export const SILBA_FULL_COURSE_PRICE = "€150";
 export const SILBA_DATES = ["1.7. - 5.7. 2026"];
 
-export const DEPTH_WORKSHOP_PRICE = "€150";
+export const DEPTH_WORKSHOP_PRICE = "€195";
 export const DEPTH_WORKSHOP_DATE = ["21.2. 2026", "28.2. 2026"];
 
 export const POOL_TRAINING_SINGLE_SESSION_PRICE = "€12";
